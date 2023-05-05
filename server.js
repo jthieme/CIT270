36d744ca7a1305ab64e7e4d162acaea0a2968e4f
Joshua Thieme <jomthieme@gmail.com>
2023-05-05 13:48:29 -0600

    added a path to resolve to

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Connected on port ${port}`)
+    console.log(`Listening on port ${port}`)
 });
 
+app.get('/', (req, res) => {
+    res.send("Node Server is here");
+})
