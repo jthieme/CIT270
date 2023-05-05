b7d4092e925591f770dd3a5402eeb0891cbfc8b8
Joshua Thieme <jomthieme@gmail.com>
2023-05-05 13:34:19 -0600

    added nodemon dep

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
-const app = express();
 
-app.listen(3000);
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Connected on port ${port}`)
+});
 
