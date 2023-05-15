38b9169e4dd83dab68bf96910b45f2d2fabb330d
Joshua Thieme <jomthieme@gmail.com>
2023-05-15 13:45:10 -0600

    connected to the db and verified user with API

--- server.js
+++ server.js
@@ -5,25 +5,34 @@ const Redis = require("redis");
 const app = express();
 const port = 3000;
 
-const redisClient = Redis.createClient();
+const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
+const redisConnect = redisClient.connect();
 
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-    redisClient.connect();
-    console.log(`Listening on port ${port}`)
+    redisConnect;
+
+    // ternary expression to log if we are connected or not
+    redisConnect ? 
+        console.log(`You're connected to Redis!\nListening on port ${port}`)
+    :   console.log("Sorry, you're not connected to Redis")
+     
 });
 
 app.get('/', (req, res) => {
     res.send("Node Server is here");
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const loginBody = req.body;
     const userName = loginBody.userName;
     const password = loginBody.password;
+    
+    const redisPassword = await redisClient.hGet("users", userName);
+    console.log(`password for ${userName}: ${redisPassword}`);
 
-    if (password === "Orange20!") {
+    if (redisPassword != null && password === redisPassword) {
         res.send(`Welcome, ${userName}!`);
     } else {
         res.status(401);
