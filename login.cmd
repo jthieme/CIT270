b62c47a5ab442d6d039872c8ab519b8f638ad5fd
Joshua Thieme <jomthieme@gmail.com>
2023-06-14 13:25:55 -0600

    removed unused line

--- login.cmd
+++ login.cmd
@@ -1,4 +1,3 @@
 @echo off
 echo "logging in"
-curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login
-@REM curl -v https://dev.stedi.me/validate/d2b1ecaa-c3f3-4d42-ae2f-825a814b06ea
+curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" https://joshthieme.cit270.com/login
